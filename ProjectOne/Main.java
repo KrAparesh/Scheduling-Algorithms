a0364dab1a716517980949b3a39e6a83bca5ed2f
Aparesh <kraparesh@gmail.com>
2023-12-31 00:05:35 +0530

    Add: Initial Commit

---
+++ ProjectOne/Main.java
@@ -0,0 +1,239 @@
+import java.util.*;
+
+// Creating a process handler that stores information about a process
+class ProcessHandler {
+    int pid, pArrival, pBurst, remTime, startTime, endTime, firstOccur;
+    boolean visited;
+
+    ProcessHandler() {
+    }
+
+    ProcessHandler(int pid, int pArrival, int pBurst) {
+        this.pid = pid;
+        this.pArrival = pArrival;
+        this.pBurst = pBurst;
+        this.remTime = pBurst;
+        this.startTime = 0;
+        this.endTime = 0;
+        this.visited = false; // Additional Data for Round Robin
+        this.firstOccur = 0; // Additional Data for Round Robin
+
+    }
+}
+
+// Creating a handler to prepare Gantt Chart for Round Robin Scheduler
+class RRHandler {
+    int sTime;
+    int eTime;
+    ProcessHandler process;
+
+    RRHandler() {
+    }
+
+    RRHandler(ProcessHandler process, int sTime, int eTime) {
+        this.process = process;
+        this.sTime = sTime;
+        this.eTime = eTime;
+    }
+}
+
+public class Main {
+
+    protected static int timeQuantum = 4;
+    protected static ArrayList<ProcessHandler> processList = new ArrayList<>();
+    protected static double avgTAT;
+    protected static double avgWT;
+    protected static double avgRT;
+
+    public static void FCFS(ArrayList<ProcessHandler> processList) {
+
+        Queue<ProcessHandler> gc = new LinkedList<>();
+
+        int timeCounter = 0;
+
+        for (ProcessHandler process : processList) {
+            process.startTime = timeCounter;
+            timeCounter += process.pBurst;
+            process.endTime = timeCounter;
+            gc.add(process);
+        }
+
+        avgTAT = getAvgTAT();
+        avgWT = getAvgWT();
+        avgRT = getAvgRT();
+
+        System.out.println("GANTT CHART \n");
+        boolean first = true;
+        while (!gc.isEmpty()) {
+            ProcessHandler p = gc.remove();
+            if (first) {
+                System.out.print(p.startTime + "  " + "P" + p.pid + "  " + p.endTime);
+                first = !first;
+            } else {
+                System.out.print("  " + "P" + p.pid + "  " + p.endTime);
+            }
+        }
+
+        System.out.println("\nAvg. Turn Around Time: " + avgTAT);
+        System.out.println("Avg. Waiting Time: " + avgWT);
+        System.out.println("Avg. Response Time: " + avgRT);
+
+    }
+
+    public static void RoundRobin(ArrayList<ProcessHandler> processList) {
+        Queue<RRHandler> gc = new LinkedList<>();
+        Queue<ProcessHandler> rq = new LinkedList<>();
+
+        int timeElapsed = 0;
+
+        // Populating the ready queue
+        for (ProcessHandler process : processList) {
+            if (process.pArrival <= timeQuantum && process.visited == false) {
+                rq.add(process);
+                process.visited = true;
+            }
+        }
+
+        while (!rq.isEmpty()) {
+            ProcessHandler p = rq.remove();
+            if (p.firstOccur == 0 && p.pid != 1)
+                p.firstOccur = timeElapsed;
+
+            if (p.remTime > timeQuantum) {
+                int tempStart = 0;
+                int tempEnd = 0;
+                p.startTime = timeElapsed;
+                p.remTime = p.remTime - timeQuantum;
+                timeElapsed = timeElapsed + timeQuantum;
+                p.endTime = timeElapsed + timeQuantum;
+                tempStart = p.startTime;
+                tempEnd = timeElapsed;
+                gc.add(new RRHandler(p, tempStart, tempEnd));
+                rq.add(p);
+            } else if (p.remTime > 0) {
+                int tempStart = 0;
+                int tempEnd = 0;
+                p.startTime = timeElapsed;
+                timeElapsed = timeElapsed + p.remTime;
+                p.endTime = timeElapsed;
+                p.remTime = 0;
+                tempStart = p.startTime;
+                tempEnd = p.endTime;
+                gc.add(new RRHandler(p, tempStart, tempEnd));
+            }
+
+            for (ProcessHandler process : processList) {
+                if (process.pArrival <= timeElapsed && process.visited == false) {
+                    rq.add(process);
+                    process.visited = true;
+                }
+            }
+
+        }
+
+        System.out.println("GANTT CHART \n");
+        boolean first = true;
+        while (!gc.isEmpty()) {
+            // System.out.println("Stuck gc");
+            ProcessHandler p = gc.peek().process;
+            int pStart = gc.peek().sTime;
+            int pEnd = gc.peek().eTime;
+            gc.remove();
+            if (first) {
+                System.out.print(pStart + "  " + "P" + p.pid + "  " + pEnd);
+                first = !first;
+            } else {
+                System.out.print("  " + "P" + p.pid + "  " + pEnd);
+            }
+        }
+
+        avgTAT = getAvgTAT();
+        avgWT = getAvgWT();
+        avgRT = getAvgRtRR();
+
+        System.out.println("\nAvg. Turn Around Time: " + avgTAT);
+        System.out.println("Avg. Waiting Time: " + avgWT);
+        System.out.println("Avg. Response Time: " + avgRT);
+    }
+
+    public static double getAvgTAT() {
+        double temp = 0;
+        for (ProcessHandler process : processList) {
+            temp += (process.endTime - process.pArrival);
+        }
+        return (temp / processList.size());
+    }
+
+    public static double getAvgWT() {
+        double temp = 0;
+        for (ProcessHandler process : processList) {
+            temp += (process.endTime - process.pArrival - process.pBurst);
+        }
+        return (temp / processList.size());
+    }
+
+    public static double getAvgRT() {
+        double temp = 0;
+        for (ProcessHandler process : processList) {
+            temp += process.startTime;
+        }
+        return (temp / processList.size());
+    }
+
+    public static double getAvgRtRR() {
+        double temp = 0;
+        for (ProcessHandler process : processList) {
+            temp += process.firstOccur;
+        }
+        return temp / processList.size();
+    }
+
+    // Round Robin
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int pid = 1;
+        // Getting process details
+        while (true) {
+            System.out.print("Enter arrival time for P" + pid + ": ");
+            int at = sc.nextInt();
+            System.out.print("Enter burst time for P" + pid + ": ");
+            int bt = sc.nextInt();
+            ProcessHandler process = new ProcessHandler(pid, at, bt);
+            processList.add(process);
+            pid++;
+            System.out.print("Add more process? (Y/N): ");
+            String choice = sc.next();
+            if (choice.equalsIgnoreCase("N"))
+                break;
+        }
+        
+        while(true) {
+            System.out.println("|============================|");
+            System.out.println("| Enter your Choice          |");
+            System.out.println("|============================|");
+            System.out.println("| 1. First Come First Serve  |");
+            System.out.println("| 2. Round Robin             |");
+            System.out.println("| 3. EXIT                    |");
+            System.out.println("|============================|");
+            
+            int choice = sc.nextInt();
+            switch(choice) {
+                case 1:
+                    FCFS(processList);
+                    break;
+                case 2:
+                    System.out.print("Enter the time quantum: ");
+                    timeQuantum = sc.nextInt();
+                    RoundRobin(processList);
+                    break;
+                case 3:
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
---
+++ ProjectTwo/Main.java
@@ -0,0 +1,207 @@
+import java.util.*;
+public class Main {
+
+    protected static int totalA, availA;
+    protected static int totalB, availB;
+    protected static int totalC, availC;
+    protected static int totalD, availD;
+
+    protected static ArrayList<ProcessHandler> processList = new ArrayList<>();
+
+    class ProcessHandler {
+        int pid;
+        int allotedA, maxNeedA, remNeedA;
+        int allotedB, maxNeedB, remNeedB;
+        int allotedC, maxNeedC, remNeedC;
+        int allotedD, maxNeedD, remNeedD;
+        boolean visited;
+
+        ProcessHandler(){}
+
+        ProcessHandler(int pid, int allotedA, int allotedB, int allotedC, int allotedD, int maxNeedA, int maxNeedB, int maxNeedC, int maxNeedD){
+            this.pid = pid;
+            this.allotedA = allotedA;
+            this.allotedB = allotedB;
+            this.allotedC = allotedC;
+            this.allotedD = allotedD;
+            this.maxNeedA = maxNeedA;
+            this.maxNeedB = maxNeedB;
+            this.maxNeedC = maxNeedC;
+            this.maxNeedD = maxNeedD;
+            this.visited = false;
+        }
+    }
+
+    public void getRemainingInstance() {
+        for(ProcessHandler process : processList) {
+            process.remNeedA = process.maxNeedA - process.allotedA;
+            process.remNeedB = process.maxNeedB - process.allotedB;
+            process.remNeedC = process.maxNeedC - process.allotedC;
+            process.remNeedD = process.maxNeedD - process.allotedD;
+        }
+    }
+
+    public void getAvailableInstance() {
+        int tempA = 0, tempB = 0, tempC = 0, tempD = 0;
+        for(ProcessHandler process : processList) {
+            tempA += process.allotedA;
+            tempB += process.allotedB;
+            tempC += process.allotedC;
+            tempD += process.allotedD;
+        }
+        availA = totalA - tempA;
+        availB = totalB - tempB;
+        availC = totalC - tempC;
+        availD = totalD - tempD;
+    }
+
+    public void printNeedMatrix() {
+        getRemainingInstance();
+        System.out.println("     PROCESS  REMAINING TIME");
+        for(ProcessHandler process: processList) {
+            System.out.println("\t" + process.pid + " \t " + process.remNeedA + " " + process.remNeedB + " " + process.remNeedC + " " + process.remNeedD);
+        }
+    }
+
+    public void Banker() {
+        Queue<ProcessHandler> freeQueue = new LinkedList<>();
+        Queue<ProcessHandler> sequence = new LinkedList<>();
+
+        getRemainingInstance();
+
+        getAvailableInstance();
+
+        while(true) {
+
+            // Calculating remaining Instance for each process
+            for(ProcessHandler process: processList) {
+                if(!process.visited) {
+                    if(availA >= process.remNeedA && availB >= process.remNeedB && availC >= process.remNeedC && availD >= process.remNeedD) {
+                        process.visited = true;
+                        freeQueue.add(process);
+                    }
+                }
+            }
+
+            if(freeQueue.isEmpty() && sequence.size() != processList.size()){
+                // Un-safe exit
+
+                System.out.println("A deadlock has been found!");
+                for(ProcessHandler p : processList) {
+                    if(!p.visited) {
+                        System.out.println("Not enough Instance to allocate to process P" + p.pid);
+                        System.out.println("Instance Required: " + p.remNeedA + " " + p.remNeedB + " " + p.remNeedC + " " + p.remNeedD);
+                        System.out.println("Instance Available: " + availA + " " + availB + " " + availC + " " + availD);
+                    }
+                }
+                return;
+            }
+
+            if(freeQueue.isEmpty() && sequence.size() == processList.size()) {
+                // Safe exit
+
+                System.out.println("SAFE EXIT!");
+                System.out.println("Process Sequence: " );
+                while(!sequence.isEmpty()) {
+                    ProcessHandler p = sequence.remove();
+                    System.out.print("P" + p.pid + " -> ");
+                }
+                System.out.println("END");
+
+                return;
+            }
+
+            while(!freeQueue.isEmpty()) {
+                ProcessHandler p = freeQueue.remove();
+
+                p.allotedA = 0;
+                p.allotedB = 0;
+                p.allotedC = 0;
+                p.allotedD = 0;
+                sequence.add(p);
+
+                getAvailableInstance();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Main ob = new Main();
+        ob.populate();
+        ob.printNeedMatrix();
+        ob.Banker();
+    }
+    
+    
+    public void populate() {
+
+        // Example 1 (3 Resources )
+
+        // ProcessHandler p1 = new ProcessHandler(1, 0, 1, 0, 7, 5, 3);
+        // ProcessHandler p2 = new ProcessHandler(2, 2, 0, 0, 3, 2, 2);
+        // ProcessHandler p3 = new ProcessHandler(3, 3, 0, 2, 9, 0, 2);
+        // ProcessHandler p4 = new ProcessHandler(4, 2, 1, 1, 4, 2, 2);
+        // ProcessHandler p5 = new ProcessHandler(5, 0, 0, 2, 5, 3, 3);
+        // processList.add(p1);
+        // processList.add(p2);
+        // processList.add(p3);
+        // processList.add(p4);
+        // processList.add(p5);
+
+        // totalA = 10;
+        // totalB = 5;
+        // totalC = 7;
+        // availA = 0;
+        // availB = 0;
+        // availC = 0;
+
+        // ====================================================================
+
+        // Example 2 ( 3 Resources )
+
+        // ProcessHandler p1 = new ProcessHandler(1, 1, 0, 1, 4, 3, 1);
+        // ProcessHandler p2 = new ProcessHandler(2, 1, 1, 2, 2, 1, 4);
+        // ProcessHandler p3 = new ProcessHandler(3, 1, 0, 3, 1, 3, 3);
+        // ProcessHandler p4 = new ProcessHandler(4, 2, 0, 0, 5, 4, 1);
+        
+        // processList.add(p1);
+        // processList.add(p2);
+        // processList.add(p3);
+        // processList.add(p4);
+    
+        // totalA = 8;
+        // totalB = 4;
+        // totalC = 6;
+        // availA = 0;
+        // availB = 0;
+        // availC = 0;
+
+        // ====================================================================
+
+        // Example 3 ( 4 Resources )
+
+        ProcessHandler p1 = new ProcessHandler(1, 0, 0, 1, 2, 0, 0, 1, 2);
+        ProcessHandler p2 = new ProcessHandler(2, 2, 0, 0, 0, 2, 7, 5, 0);
+        ProcessHandler p3 = new ProcessHandler(3, 0, 0, 3, 4, 6, 6, 5, 6);
+        ProcessHandler p4 = new ProcessHandler(4, 2, 3, 5, 4, 4, 3, 5, 6);
+        ProcessHandler p5 = new ProcessHandler(5, 0, 3, 3, 2, 0, 6, 5, 2);
+
+        processList.add(p1);
+        processList.add(p2);
+        processList.add(p3);
+        processList.add(p4);
+        processList.add(p5);
+
+        totalA = 6;
+        totalB = 7;
+        totalC = 12;
+        totalD = 12;
+
+        availA = 0;
+        availB = 0;
+        availC = 0;
+        availD = 0;
+    }
+
+}
